d52292322910ae83215d7cddee146171a1e2de7b
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-12-19 13:39:59 +0100

    create project

---
+++ ToTheBeat/Intermediate/Build/Win64/UE4Editor/Inc/ToTheBeat/ToTheBeatGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef TOTHEBEAT_ToTheBeatGameModeBase_generated_h
+#error "ToTheBeatGameModeBase.generated.h already included, missing '#pragma once' in ToTheBeatGameModeBase.h"
+#endif
+#define TOTHEBEAT_ToTheBeatGameModeBase_generated_h
+
+#define ToTheBeat_Source_ToTheBeat_ToTheBeatGameModeBase_h_15_SPARSE_DATA
+#define ToTheBeat_Source_ToTheBeat_ToTheBeatGameModeBase_h_15_RPC_WRAPPERS
+#define ToTheBeat_Source_ToTheBeat_ToTheBeatGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define ToTheBeat_Source_ToTheBeat_ToTheBeatGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAToTheBeatGameModeBase(); \
+	friend struct Z_Construct_UClass_AToTheBeatGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AToTheBeatGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/ToTheBeat"), NO_API) \
+	DECLARE_SERIALIZER(AToTheBeatGameModeBase)
+
+
+#define ToTheBeat_Source_ToTheBeat_ToTheBeatGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAToTheBeatGameModeBase(); \
+	friend struct Z_Construct_UClass_AToTheBeatGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AToTheBeatGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/ToTheBeat"), NO_API) \
+	DECLARE_SERIALIZER(AToTheBeatGameModeBase)
+
+
+#define ToTheBeat_Source_ToTheBeat_ToTheBeatGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AToTheBeatGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AToTheBeatGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AToTheBeatGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AToTheBeatGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AToTheBeatGameModeBase(AToTheBeatGameModeBase&&); \
+	NO_API AToTheBeatGameModeBase(const AToTheBeatGameModeBase&); \
+public:
+
+
+#define ToTheBeat_Source_ToTheBeat_ToTheBeatGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AToTheBeatGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AToTheBeatGameModeBase(AToTheBeatGameModeBase&&); \
+	NO_API AToTheBeatGameModeBase(const AToTheBeatGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AToTheBeatGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AToTheBeatGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AToTheBeatGameModeBase)
+
+
+#define ToTheBeat_Source_ToTheBeat_ToTheBeatGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define ToTheBeat_Source_ToTheBeat_ToTheBeatGameModeBase_h_12_PROLOG
+#define ToTheBeat_Source_ToTheBeat_ToTheBeatGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ToTheBeat_Source_ToTheBeat_ToTheBeatGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	ToTheBeat_Source_ToTheBeat_ToTheBeatGameModeBase_h_15_SPARSE_DATA \
+	ToTheBeat_Source_ToTheBeat_ToTheBeatGameModeBase_h_15_RPC_WRAPPERS \
+	ToTheBeat_Source_ToTheBeat_ToTheBeatGameModeBase_h_15_INCLASS \
+	ToTheBeat_Source_ToTheBeat_ToTheBeatGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define ToTheBeat_Source_ToTheBeat_ToTheBeatGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ToTheBeat_Source_ToTheBeat_ToTheBeatGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	ToTheBeat_Source_ToTheBeat_ToTheBeatGameModeBase_h_15_SPARSE_DATA \
+	ToTheBeat_Source_ToTheBeat_ToTheBeatGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	ToTheBeat_Source_ToTheBeat_ToTheBeatGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	ToTheBeat_Source_ToTheBeat_ToTheBeatGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> TOTHEBEAT_API UClass* StaticClass<class AToTheBeatGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID ToTheBeat_Source_ToTheBeat_ToTheBeatGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
