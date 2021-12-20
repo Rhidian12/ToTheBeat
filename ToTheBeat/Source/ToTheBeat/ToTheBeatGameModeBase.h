dd7e6daa5f76d1f68ad66b74cfe528ed952eb4ba
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-12-20 12:10:18 +0100

    made new project

---
+++ ToTheBeat/Source/ToTheBeat/ToTheBeatGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ToTheBeatGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOTHEBEAT_API AToTheBeatGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
