5a5455d638dbdb22935d0fd5685eb69cf848795e
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-12-27 13:05:17 +0100

    idk

--- ToTheBeat/Source/ToTheBeat/Private/MusicBlockSpawner.cpp
+++ ToTheBeat/Source/ToTheBeat/Private/MusicBlockSpawner.cpp
@@ -6,21 +6,59 @@
 // Sets default values
 AMusicBlockSpawner::AMusicBlockSpawner()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	FJsonSerializableArray fileContents{};
-	FFileHelper::LoadFileToStringArray(fileContents, TEXT("SongBeats/Undertale - Megalovania.txt"));
+	FString path{ FPaths::ProjectContentDir() };
+	path.Append("SongBeats/Undertale - Megalovania.txt");
 
-	for (const auto& element : fileContents)
-		UE_LOG(LogTemp, Warning, TEXT("%s"), element);
+	IPlatformFile& FileManager = FPlatformFileManager::Get().GetPlatformFile();
+
+	FString fileContent;
+	if (FileManager.FileExists(*path))
+	{
+		if (FFileHelper::LoadFileToString(fileContent, *path, FFileHelper::EHashOptions::None))
+		{
+			//UE_LOG(LogTemp, Warning, TEXT("FileManipulation: Text From File: %s"), *fileContent);
+			UE_LOG(LogTemp, Warning, TEXT("Successfully loaded file"));
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("FileManipulation: Did not load text from file"));
+		}
+	}
+
+	 for (int32 i{}; i < fileContent.Len();)
+	 {
+	 	const int32 newLineLocation{ fileContent.Find(&FChar::NextLine, ESearchCase::Type::IgnoreCase, ESearchDir::Type::FromStart, i) };
+	 
+	 	if (newLineLocation != INDEX_NONE)
+	 	{
+	 		FString line{ fileContent.Mid(i, newLineLocation) };
+
+			UE_LOG(LogTemp, Warning, TEXT("Line %i :, %s"), i, *line);
+	 
+	 		if (line.Find(TEXT("#"), ESearchCase::Type::IgnoreCase, ESearchDir::Type::FromStart, -1) != INDEX_NONE)
+	 		{
+	 			i = newLineLocation + 1;
+	 			continue;
+	 		}
+	 		else
+	 		{
+	 			
+	 			UE_LOG(LogTemp, Warning, TEXT(""), *line);
+	 
+	 			i = newLineLocation + 1;
+	 		}
+	 	}
+	 }
 }
 
 // Called when the game starts or when spawned
 void AMusicBlockSpawner::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
